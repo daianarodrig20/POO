53b529d0a9c82846064fba2b6102ec5855f516a9
Daiana Rodriguez <daianarodriguez020@gmail.com>
2021-11-22 02:47:35 -0300

    add login

---
+++ loginApplication/src/test/java/loginApplication/loginApplication/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package loginApplication.loginApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
